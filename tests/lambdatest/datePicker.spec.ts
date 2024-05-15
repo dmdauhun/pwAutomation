122714631dd0e61f2a93081c801c1a2e7f54e4a5
Dzmitry Dauhun <51424255+dmdauhun@users.noreply.github.com>
2024-05-14 23:19:06 -0400

    Datepicker (#3)
    * Form Page crated
    
    * changes in config

---
+++ tests/lambdatest/datePicker.spec.ts
@@ -0,0 +1,13 @@
+import {test as it } from 'playwright/test';
+import {DatePicker} from '../../page_object/DatePicker'
+
+
+it.describe.only('Date picker',()=>{
+    it('working with date picker',async ({page})=>{
+
+        let datePicker=new DatePicker(page)
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-date-picker-demo')
+        await datePicker.verifyHeader()
+        await datePicker.dateFromToday()
+    })
+})
