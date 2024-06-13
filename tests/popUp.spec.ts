b1d8ee60525e488013f799d3d24b9a72ace38aa8
Dzmitry Dauhun <51424255+dmdauhun@users.noreply.github.com>
2024-06-13 19:31:21 -0400

    Iframe (#9)
    * iframe
    
    * iframe udates

--- tests/popUp.spec.ts
+++ tests/popUp.spec.ts
@@ -1,10 +1,18 @@
 import {expect, test} from "@playwright/test"
 
-test.describe('HIDDEN ELEMENTS', () =>{
+test.describe('POPUP WINDOW accept', () =>{
     test('popUP window', async ({page})=>{
         await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
-        page.on('dialog',dialog=>dialog.accept())
-        page.locator
+        //await page.pause()
+        page.on('dialog',el=>el.accept())
+        await  page.locator('#confirmbtn').click()
+    })
+
+    test('popUP window dismiss', async ({page})=>{
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+       // await page.pause()
+        page.on('dialog',el=>el.dismiss())
+        await  page.locator('#confirmbtn').click()
     })
 })
     
