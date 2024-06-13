c2d330a0c7778eb45c9cf3a7b95372a7d770ec4a
dmdauhun <dm.dauhun@gmail.com>
2024-06-13 19:28:32 -0400

    iframe udates

---
+++ tests/network/intercept.spec.ts
@@ -0,0 +1,16 @@
+import {expect, test} from '@playwright/test'
+
+test('intercept',async ({page})=>{
+await page.route('**\/*.{png,jpg,jpeg,svg}',(body)=>{
+   // console.log(request)
+    if(body.request().resourceType()==='image'){
+        body.abort()
+    }else{
+        body.continue()
+    }
+})
+    //await page.pause()
+    await page.goto('https://coding.pasv.us/')
+    //await page.pause()
+
+})
---
+++ tests/network/pageEvents.spec.ts
@@ -0,0 +1,20 @@
+import {expect, test} from '@playwright/test'
+
+test('Pageerror',async ({page})=>{
+await page.route('**\/*.{png,jpg,jpeg,svg}',(body)=>{
+   // console.log(request)
+    if(body.request().resourceType()==='image'){
+        body.abort()
+    }else{
+        body.continue()
+    }
+})
+   page.on('pageerror',error=>{
+    console.log('error: ${error}')
+
+   })
+   await page.pause()
+await page.goto('https://demoqa.com')
+//await page.goto('data:text/html,<script>throw new Error("Test")</script>');
+await page.pause()
+})
